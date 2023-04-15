ab7ab81698dc273f0539c21a3880d84ef3e67214
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2023-04-15 11:39:16 -0400

    intiated useState and use Effect

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './src/styles'
 
 export default function App() {
+  const [jokes, setJokes] = useState()
+  useEffect(() => {
+    
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#262262',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+        color: 'pink',
+        fontSize: 48,
+        width: '90%'
+    }
+  });
+  
